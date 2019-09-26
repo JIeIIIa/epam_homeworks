05b80269ea239d47ea51a9a033fd7373230911ef
Oleksii Onishchenko <o.onishchenko@finik.pro>
2019-09-26 14:57:17 +0300

    HW-05_01 - Use ClassNameConverter in BitCounterService. Change method's signature in BitCounterService.

--- src/main/java/epam/lecture01/bitcounter/BitCounterService.java
+++ src/main/java/epam/lecture01/bitcounter/BitCounterService.java
@@ -1,14 +1,22 @@
 package epam.lecture01.bitcounter;
 
 public class BitCounterService {
+
+    private ClassNameConverter classNameConverter;
+
+    public BitCounterService(ClassNameConverter classNameConverter) {
+        this.classNameConverter = classNameConverter;
+    }
+
     /**
      * Finds total bits in a given class
      *
-     * @param clazz {@link Class} for calculate. Expected byte, short, int or long
+     * @param className for calculate. Expected "byte", "short", "int" or "long"
      * @return total bits in a given class
      * @throws IllegalArgumentException if class do not equal to one of the expected class
      */
-    int proceed(Class clazz) {
+    int proceed(String className) {
+        Class clazz = classNameConverter.convert(className);
         int count = 1;
         long number = 1;
         do {
