2cd2e4f08d2e397b06e17903d20b970eae87288a
Oleksii Onishchenko <o.onishchenko@finik.pro>
2019-09-27 11:34:26 +0300

    L-01 - Add Lily.

---
+++ src/main/java/mape/lab01/flower/Lily.java
@@ -0,0 +1,37 @@
+package mape.lab01.flower;
+
+import java.util.Objects;
+
+public class Lily extends AbstractFlower{
+    private final int size;
+
+    public Lily(int size) {
+        super("Lily");
+        this.size = size;
+    }
+
+    public int getSize() {
+        return size;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        if (!super.equals(o)) return false;
+        Lily lily = (Lily) o;
+        return size == lily.size;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(super.hashCode(), size);
+    }
+
+    @Override
+    public String toString() {
+        return "Lily{ " + super.toString() +
+            ", size=" + size +
+            '}';
+    }
+}
