7f02611f5bf27fe4618fa6de72a217188bc06ae7
Oleksii Onishchenko <o.onishchenko@finik.pro>
2019-09-27 13:55:00 +0300

    L-01 - Add Bouquet to store information about flowers and accessories.

---
+++ src/main/java/mape/lab01/bouquet/Bouquet.java
@@ -0,0 +1,74 @@
+package mape.lab01.bouquet;
+
+import mape.lab01.Valuable;
+import mape.lab01.flower.AbstractFlower;
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+public class Bouquet {
+    private final List<AbstractFlower> flowers = new ArrayList<>();
+    private final List<Accessory> accessories = new ArrayList<>();
+
+    public void addFlower(AbstractFlower flower) {
+        flowers.add(flower);
+    }
+
+    public AbstractFlower removeFlower(int index) {
+        return flowers.remove(index);
+    }
+
+    public void addAccessory(Accessory accessory) {
+        accessories.add(accessory);
+    }
+
+    public Accessory removeAccessory(int index) {
+        return accessories.remove(index);
+    }
+
+    public int totalPrice() {
+        return Stream
+            .concat(flowers.stream(), accessories.stream())
+            .map(Valuable::getPrice)
+            .reduce(0, Integer::sum);
+    }
+
+    public String printByFreshness() {
+        Comparator<AbstractFlower> freshLevelComparator = Comparator
+            .comparingInt(AbstractFlower::getFreshnessLevel)
+            .reversed();
+        List<AbstractFlower> sorted = new ArrayList<>(flowers);
+        sorted.sort(freshLevelComparator);
+
+        return toString(sorted, accessories);
+    }
+
+    public List<AbstractFlower> filter(int minLength, int maxLength) {
+        if (minLength > maxLength) {
+            throw new IllegalArgumentException("Min length must be less or equal to max length");
+        }
+        return flowers
+            .stream()
+            .filter(f -> minLength <= f.getLength() && f.getLength() <= maxLength)
+            .collect(Collectors.toList());
+    }
+
+    @Override
+    public String toString() {
+        return toString(flowers, accessories);
+    }
+
+    private String toString(List<AbstractFlower> flowers, List<Accessory> accessories) {
+        StringBuilder sb = new StringBuilder("Bouquet\n");
+        for (AbstractFlower flower : flowers) {
+            sb.append("   ").append(flowers).append("\n");
+        }
+        sb.append("with accessories\n");
+        for (Accessory accessory : accessories) {
+            sb.append("   ").append(accessory).append("\n");
+        }
+        return sb.toString();
+    }
+}
